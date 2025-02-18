58ba2941a8e4725426c07cb79c7b669a38947412
Paul Fisher <paulfisher53@gmail.com>
2025-02-19 09:32:43 +1100

    🐛 Update token estimate function

--- out/extension.js
+++ out/extension.js
@@ -64,8 +64,12 @@ const CONFIG = {
     MODEL: "ollamaModel",
 };
 function stringToTokenCount(text) {
-    const tokenLength = 4;
-    return text.length / tokenLength;
+    const tokens = text.match(/[^\r\n\p{L}\p{N}]?[\p{Lu}\p{Lt}\p{Lm}\p{Lo}\p{M}]*[\p{Ll}\p{Lm}\p{Lo}\p{M}]+('s|'S|'t|'T|'re|'rE|'Re|'RE|'ve|'vE|'Ve|'VE|'m|'M|'ll|'lL|'Ll|'LL|'d|'D)?|[^\r\n\p{L}\p{N}]?[\p{Lu}\p{Lt}\p{Lm}\p{Lo}\p{M}]+[\p{Ll}\p{Lm}\p{Lo}\p{M}]*('s|'S|'t|'T|'re|'rE|'Re|'RE|'ve|'vE|'Ve|'VE|'m|'M|'ll|'lL|'Ll|'LL|'d|'D)?|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n/]*|\s*[\r\n]+|\s+(?!\S)|\s+/gu);
+    let tokenCount = 0;
+    tokens?.forEach((token) => {
+        tokenCount += Math.ceil(Math.min(1, token.trim().length) / 4);
+    });
+    return tokenCount;
 }
 function activate(context) {
     const disposable = vscode.commands.registerCommand("zoomiverse-ai.start", () => {
